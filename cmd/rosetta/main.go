49cebb5ec2a725f5e1bbc4d1fefed8d2dea5047d
Eric Myhre <hash@exultant.us>
2017-12-11 21:42:02 +0100

    Embed longer description in the command helptext.
    Mostly content regurgitated from the readme in slightly different conjugation.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- cmd/rosetta/main.go
+++ cmd/rosetta/main.go
@@ -20,6 +20,18 @@ func Main(args []string, stdin io.Reader, stdout, stderr io.Writer) int {
 	app := cli.NewApp()
 	app.Name = "rosetta"
 	app.Usage = "a tool for storing secrets with deterministic encryption."
+	app.Description = "Rosetta is a simple, scriptable file encryption tool." +
+		"\n" +
+		"\n   Rosetta works with symmetric keys, saves your encrypted secrets" +
+		"\n   in the standard PEM (RFC 1421) plaintext format for ease of use" +
+		"\n   and simple copy-paste interaction, and includes helpful features" +
+		"\n   like built-in password-derived key generation." +
+		"\n" +
+		"\n   Rosetta is designed to produce encryption that is *deterministic*" +
+		"\n   from the content and keys.  This means Rosetta is can be used" +
+		"\n   efficiently for storing secrets in e.g. a git repository; it" +
+		"\n   will only generate diffs when the content is changed.  Go ahead." +
+		"\n   Commit your ciphertext."
 	app.Commands = []cli.Command{
 		cli.Command{
 			Category: "basic",
